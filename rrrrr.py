5a1fc31fa4f1c50c105af6cec9c58bf1bb059810
dev312004 <devi31032004@gmail.com>
2026-04-19 11:06:08 +0530

    inser

--- rrrrr.py
+++ rrrrr.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
-data = pd.read_csv('urls.csv')
+data = pd.read_csv("urls.csv")
 print("Dataset Sample:")
 print(data.head())
 def clean_url(url):
